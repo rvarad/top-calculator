d283657d6ef259eb50521dcb3d16511b5cbb6c3c
Varad Rampurikar <rampurikarvarad@gmail.com>
2022-11-07 11:01:37 +0530

    created add,sbutract,multiply,divide functions

---
+++ app.js
@@ -0,0 +1,19 @@
+let addition = function (a, b) {
+    return (a + b);
+}
+
+let subtraction = function (a, b) {
+    return (a - b);
+}
+
+let multiplication = function (a, b) {
+    return (a * b);
+}
+
+let division = function (a, b) {
+    return (a / b);
+}
+
+// let operate = function (op1, op2) {
+
+// }
